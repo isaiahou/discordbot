54ea01885c0a66b8feb41f558dd55a83aea25afa
Isaiah Ou <isaiahou@berkeley.edu>
2022-06-04 13:09:17 -0700

    Update main.py
    Updated main.py

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ import discord
 bot = discord.Client()
 
 @bot.event
+async def on_ready():
+    print(f'Bot logged in as {bot.user}')
+
+@bot.event
 async def on_message(message):
+    if message.author == bot.user:
+        return
     if message.content == "$hello":
         await message.channel.send("Hello! I am a bot.")
+    if message.content == "what's something that runs fast and plays basketball well":
+        await message.channel.send("a stinkin nigger")
 
 bot.run("OTgyNzE4MjExNzA1NDE3NzYz.Ga0Rf9.6xhPWsHHvk_nP5acfu1MiE--vun24XKCReSRGo")
