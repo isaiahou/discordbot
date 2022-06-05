f0348ac7cde16b90e7d764ae0dfbc2176fc716a9
Isaiah Ou <isaiahou@berkeley.edu>
2022-06-04 23:00:33 -0700

    Bot updated

--- embed_commands.py
+++ embed_commands.py
@@ -8,9 +8,9 @@ class embed(commands.Cog):
     @commands.command()
     async def embed(self, ctx):
         embed = discord.Embed(title = _title, url = _url, description = _description, color = _color)
-        await ctx.send(embed = embed)
         embed.set_author(name = ctx.author.display_name, url = _url2, icon_url = ctx.author.avatar_url)
         embed.set_thumbnail(url = "https://vignette.wikia.nocookie.net/clashroyale/images/4/4f/Laughing_Goblin.png/revision/latest/scale-to-width-down/340?cb=20180717203752")
+        await ctx.send(embed = embed)
 
      
 
@@ -19,7 +19,7 @@ _url = "https://github.com/isaiahou"
 _description = "Checkout my GitHub profile!"
 _color = discord.Color.red()
 _name = "Salted"
-_url2 = "google.com"
+_url2 = "https://google.com"
 _icon_url = "icon url"
 _thumbnail = "thumbnail"
 
